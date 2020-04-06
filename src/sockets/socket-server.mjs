c58f212b9e481f2254c887bd3a964d9402ca1d94
i-akash <i.akash.se@gmail.com>
2020-04-06 19:54:57 +0600

    error handling

--- src/sockets/socket-server.mjs
+++ src/sockets/socket-server.mjs
@@ -1,5 +1,5 @@
 import io from 'socket.io'
-import { promisify } from 'util'
+import { promisify, log } from 'util'
 import redis from 'redis'
 import {
     ALL_ROOM,
@@ -28,6 +28,7 @@ const SocketServer = function () { }
 SocketServer.prototype.createServer = function () {
     this.redisClient = redis.createClient()
     this.redisClient.get = promisify(this.redisClient.get)
+    this.redisClient.set = promisify(this.redisClient.set)
     this.chatApp = io(80);
 }
 
@@ -45,7 +46,6 @@ SocketServer.prototype.use = function () {
 
 SocketServer.prototype.listen = function () {
     this.chatApp.on(CONNECT, (socket) => {
-
         this.getAllRoom(socket)
         this.getAllClientOf(socket)
 
@@ -74,19 +74,23 @@ SocketServer.prototype.listen = function () {
 
 // rooms start
 SocketServer.prototype.getAllRoom = function (socket) {
-    socket.on(ALL_ROOM, (nsp = '/', onResponse) => {
+    socket.on(ALL_ROOM, (nsp = '/', responseCallback) => {
         let allRooms = Object.keys(this.chatApp.of(nsp).adapter.rooms)
-        onResponse(allRooms)
+        responseCallback(allRooms)
     })
 }
 
 
 SocketServer.prototype.getAllClientOf = function (socket) {
-    socket.on(ALL_CLIENT_IN_ROOM, (room, callback) => {
+    socket.on(ALL_CLIENT_IN_ROOM, (room, responseCallback) => {
         this.chatApp.in(room).clients((error, clientList) => {
-            if (error) throw error
+            if (error) {
+                console.log("All CLIENT ROOM ERROR : ", error);
+                responseCallback([])
+                return;
+            }
             let usersInfo = clientList.map(clientID => this.redisClient.get(clientID))
-            Promise.all(usersInfo).then(users => callback(users.map(user => JSON.parse(user))))
+            Promise.all(usersInfo).then(users => responseCallback(users.map(user => JSON.parse(user))))
         })
     })
 }
@@ -95,21 +99,25 @@ SocketServer.prototype.getAllClientOf = function (socket) {
 SocketServer.prototype.onJoinRoom = function (socket) {
     socket.on(JOIN_ROOM, (userInfo, ackCallback) => {
         socket.join(userInfo.userRoom, (error) => {
-            if (error) throw error
-
+            if (error) {
+                console.log("JOIN ROOM ERROR : ", error);
+                ackCallback(error.message)
+            }
             userInfo.userId = socket.id
-            this.redisClient.set(socket.id, JSON.stringify(userInfo), redis.print)
-            ackCallback(Object.keys(socket.rooms))
+            this.redisClient.set(socket.id, JSON.stringify(userInfo))
             this.broadcastToRoom(NEW_JOINING_ROOM, userInfo.userRoom, userInfo)
+            ackCallback()
         })
     })
 }
 
 SocketServer.prototype.onLeaveRoom = function (socket) {
     socket.on(LEAVE_ROOM, (room, ackCallback) => {
-        console.log(room);
         socket.leave(room, (error) => {
-            if (error) throw error
+            if (error) {
+                console.log("LEAVE ROOM ERROR : ", error);
+                ackCallback(error.message)
+            }
             ackCallback()
             this.broadcastToRoom(LEFT_ROOM, room, socket.id)
         })
@@ -127,7 +135,6 @@ SocketServer.prototype.onDisconnecting = function (socket) {
 
 SocketServer.prototype.onTypingMessage = function (socket) {
     socket.on(MESSAGE_TYPING, (room, userIfo) => {
-        console.log(MESSAGE_TYPING, userIfo);
         socket.to(room).emit(MESSAGE_TYPING, userIfo)
     })
 }
@@ -144,7 +151,10 @@ SocketServer.prototype.onNewMessage = function (socket) {
 SocketServer.prototype.getAllMessages = function (socket) {
     socket.on(ALL_MESSAGE, (room, callback) => {
         this.redisClient.lrange(`${room}:messages`, 0, -1, (error, messages = []) => {
-            if (error) throw error
+            if (error) {
+                console.log("GET ALL SMS ERROR : ", error.message);
+                callback([])
+            }
             let parsedMessages = messages.map(message => JSON.parse(message))
             callback(parsedMessages)
         })
@@ -154,10 +164,10 @@ SocketServer.prototype.getAllMessages = function (socket) {
 SocketServer.prototype.onAddProblem = function (socket) {
     socket.on(NEW_PROBLEM, ({ room, payload }, ackCallback) => {
         payload.timeStamp = new Date()
-        this.redisClient.set(`${room}:problem`, JSON.stringify(payload), redis.print)
-        // this.redisClient.rpush([`${room}:problems`, JSON.stringify(payload)], redis.print)
-        ackCallback()
-        this.broadcastToRoom(NEW_PROBLEM, room, payload)
+        this.redisClient.set(`${room}:problem`, JSON.stringify(payload)).then(res => {
+            ackCallback()
+            this.broadcastToRoom(NEW_PROBLEM, room, payload)
+        }).catch(error => ackCallback(error.message))
     })
 }
 
@@ -184,7 +194,10 @@ SocketServer.prototype.onAddSolution = function (socket) {
 SocketServer.prototype.getAllSolution = function (socket) {
     socket.on(ALL_SOLUTION, (room, callback) => {
         this.redisClient.lrange(`${room}:solutions`, 0, -1, (error, solutions = []) => {
-            if (error) throw error
+            if (error) {
+                console.log("GET ALL SOLUTION ERROR : ", error);
+                callback([])
+            }
             let parsedSolutions = solutions.map(solution => JSON.parse(solution))
             callback(parsedSolutions)
         })
